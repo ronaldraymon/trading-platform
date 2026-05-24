ae33f1a13627fe55e6878473af8837ff701b46bf
ronaldraymon <ronaldraymon@gmail.com>
2026-05-25 01:15:20 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT, 10) || 5000;
 
 app.use(cors());
 app.use(express.json());
